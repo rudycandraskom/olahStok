4d4e9dce1f387414355a22851f57dc58276cbf3f
rudycandraskom <rudy.ben.candra@gmail.com>
2022-12-05 17:25:40 +0700

    Sederhanakan Program

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-/**Import Modules */
-const fs = require("fs");
 const initializeApp = require("firebase/app");
-const getDatabase = require("firebase/database");
-const { ref, set, goOffline } = require("firebase/database");
-const csv = require('csvtojson');
+const { ref, set, goOffline, getDatabase } = require("firebase/database");
+const fs = require("fs");
+const { getStokFinal, getStokMentah, getStructured, csvJSON, delay, Barang, ObjStok } = require("./module.js");
+
+const stokMaster = csvJSON(fs.readFileSync("stokMaster.csv", "utf-8"), ",");
+const stokSAP = csvJSON(getStructured(fs.readFileSync("stokMentah.csv", "utf-8")), "|");
+const stokMentah = getStokMentah(stokSAP)
+const stokFinal = getStokFinal(stokMentah, stokMaster)
 
 /** Koneksi Firebase App dan Firebase Database */
 const firebaseConfig = {
@@ -17,107 +20,14 @@ const firebaseConfig = {
     measurementId: "G-JJ9C7YRS8P"
 };
 const app = initializeApp.initializeApp(firebaseConfig);
-const database = getDatabase.getDatabase();
+const database = getDatabase();
 
-/**Class ObjStok dan Barang */
-class ObjStok {
-    constructor(lastUpdate, listBarang) {
-        this.lastUpdate = lastUpdate;
-        this.listBarang = listBarang;
-    }
-}
-class Barang {
-    constructor(kodeProduk, namaProduk, sisaStok) {
-        this.kodeProduk = kodeProduk;
-        this.namaProduk = namaProduk;
-        this.sisaStok = sisaStok;
-    }
-}
-
-/** Fungsi Delay */
-function delay(detik) {
-    return new Promise(resolve => setTimeout(resolve, detik * 1000 || DEF_DELAY));
-}
-
-/** Konversi stokMentah.csv jadi stokMentah.json */
-function konversiStokMentahCSVtoJSON() {
-    const csvStokMentah = 'stokMentah.csv';
-    csv({ noheader: true })
-        .fromFile(csvStokMentah)
-        .then((jsonObj) => {
-            lastUpdate = jsonObj[0].field3;
-            let listBarang = jsonObj.map(value => {
-                return new Barang(value.field9, value.field10, value.field12);
-            })
-            let dataStok = new ObjStok(lastUpdate, listBarang);
-
-            try {
-                fs.writeFileSync('stokMentah.json', JSON.stringify(dataStok, null, 4));
-            } catch (err) {
-                console.error(err);
-            }
-        });
-};
-
-/** Konversi stokMaster.csv menjadi stokMaster.json */
-function konversiStokMasterCSVtoJSON() {
-    const csvStokMaster = "StokMaster.csv";
-    csv()
-        .fromFile(csvStokMaster)
-        .then((jsonObj) => {
-            let objStokMaster = jsonObj.map(value => {
-                return value;
-            });
-            try {
-                fs.writeFileSync('stokMaster.json', JSON.stringify(objStokMaster, null, 4));
-            } catch (err) {
-                console.error(err);
-            }
-        });
-};
-
-/** Proses stokMaster combine StokMentah menjadi stokFinal */
-function combine(stokMentah, stokMaster) {
-    var hasil = {};
-    for (let i = 0; i < stokMaster.length; i++) {
-        for (let j = 0; j < stokMentah.listBarang.length; j++) {
-            if (stokMaster[i].kodeBarang === stokMentah.listBarang[j].kodeProduk) {
-                let intIsiPerBox = parseInt(stokMaster[i].isiPerBox.replace(/,/g, ''));
-                let intSisaStokMentah = parseInt(stokMentah.listBarang[j].sisaStok.replace(/,/g, ''));;
-                stokMaster[i].sisaStok = `${Math.floor(intSisaStokMentah / intIsiPerBox)} BOX\n${intSisaStokMentah % intIsiPerBox} ROLL`;
-            };
-        }
-
-        hasil[i] = {
-            kodeProduk: stokMaster[i].kodeBarang,
-            namaProduk: stokMaster[i].namaBarang,
-            sisaStok: stokMaster[i].sisaStok
-        }
-    }
-    return new ObjStok(stokMentah.lastUpdate, hasil);
-}
-
+console.log("Loading....");
 (async() => {
-    konversiStokMentahCSVtoJSON();
-    console.log("stokMentah.json (1of4)");
     await delay(3);
-    konversiStokMasterCSVtoJSON();
-    console.log("stokMaster.json (2of4)");
-    await delay(3);
-    try {
-        const jsonStokMaster = fs.readFileSync("StokMaster.json", { encoding: 'utf8', flag: 'r' });
-        const jsonStokMentah = fs.readFileSync("StokMentah.json", { encoding: 'utf8', flag: 'r' });
-        var stokFinal = combine(JSON.parse(jsonStokMentah), JSON.parse(jsonStokMaster));
-        fs.writeFileSync('stokFinal.json', JSON.stringify(stokFinal, null, 4));
-        console.log("stokFinal.json (3of4)");
-    } catch (error) {
-        console.log(error);
-    }
-    await delay(3)
-    stokFinal = fs.readFileSync('../olahStok/stokFinal.json', { encoding: 'utf8', flag: 'r' });
-    set(ref(database, "stokFinal"), JSON.parse(stokFinal));
+    set(ref(database, "stokFinal"), stokFinal);
     console.log("Stok Final sedang di Upload....");
-    await delay(5);
-    console.log("Stok Final berhasil di-Upload (4/4)");
+    await delay(3);
+    console.log("Stok Final berhasil di-Upload ");
     goOffline(database);
 })();
---
+++ module.js
@@ -0,0 +1,78 @@
+/**Class ObjStok dan Barang */
+class ObjStok {
+    constructor(lastUpdate, listBarang) {
+        this.lastUpdate = lastUpdate;
+        this.listBarang = listBarang;
+    }
+}
+class Barang {
+    constructor(kodeProduk, namaProduk, sisaStok) {
+        this.kodeProduk = kodeProduk;
+        this.namaProduk = namaProduk;
+        this.sisaStok = sisaStok;
+    }
+}
+
+/** Fungsi Delay */
+function delay(detik) {
+    return new Promise(resolve => setTimeout(resolve, detik * 1000 || DEF_DELAY));
+}
+
+function csvJSON(csv, spliter) {
+    const lines = csv.split('\r\n')
+    const result = []
+    const headers = lines[0].split(spliter)
+
+    for (let i = 1; i < lines.length; i++) {
+        if (!lines[i])
+            continue
+        const obj = {}
+        const currentline = lines[i].split(spliter)
+        for (let j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j]
+        }
+        result.push(obj)
+    }
+    return result
+}
+
+function getStructured(stringCSV) {
+    let header = 'f1|f2|f3|f4|f5|f6|f7|f8|f9|f10|f11|f12|f13'
+    stringCSV = stringCSV.replaceAll(`","`, '|')
+    stringCSV = stringCSV.replaceAll(`",`, '|')
+    stringCSV = stringCSV.replaceAll(`,"`, '|')
+    stringCSV = stringCSV.replaceAll(' "', '\r\n')
+    stringCSV = stringCSV.replaceAll('"', ``)
+    stringCSV = header + '\r\n' + "," + stringCSV;
+    return stringCSV;
+}
+
+function getStokMentah(stokSAP) {
+    let lastUpdate = stokSAP[0].f3;
+    let a = []
+    let listBarang = stokSAP.map(value => {
+        return new Barang(value.f9, value.f10, value.f12);
+    })
+    return new ObjStok(lastUpdate, listBarang);
+}
+
+function getStokFinal(stokMentah, stokMaster) {
+    var hasil = {};
+    for (let i = 0; i < stokMaster.length; i++) {
+        for (let j = 0; j < stokMentah.listBarang.length; j++) {
+            if (stokMaster[i].kodeBarang === stokMentah.listBarang[j].kodeProduk) {
+                let intIsiPerBox = parseInt(stokMaster[i].isiPerBox.replace(/,/g, ''));
+                let intSisaStokMentah = parseInt(stokMentah.listBarang[j].sisaStok.replace(/,/g, ''));;
+                stokMaster[i].sisaStok = `${Math.floor(intSisaStokMentah / intIsiPerBox)} BOX\n${intSisaStokMentah % intIsiPerBox} ROLL`;
+            };
+        }
+
+        hasil[i] = {
+            kodeProduk: stokMaster[i].kodeBarang,
+            namaProduk: stokMaster[i].namaBarang,
+            sisaStok: stokMaster[i].sisaStok
+        }
+    }
+    return new ObjStok(stokMentah.lastUpdate, hasil);
+}
+module.exports = { getStokFinal, getStokMentah, getStructured, csvJSON, delay, Barang, ObjStok }
